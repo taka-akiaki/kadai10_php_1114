421ccbcc86872ba25a67f3f4cbf101254d3d1aaa
taka-akiaki <116429888+taka-akiaki@users.noreply.github.com>
2023-01-19 19:05:34 +0900

    Delete kadai10_php_0114 directory

--- kadai10_php_0114/common/footer.php
+++
@@ -1,8 +0,0 @@
-<?php
-$footer = <<<EOM
-<footer class="footer mt-auto py-3 bg-light">
-<div class="container">
-<span class="text-muted"><a href="admin/login.php">管理者はこちら</a></span>
-</div>
-</footer>
-EOM;
